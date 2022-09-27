9d9cb27b86dc7cde8141021bf09e2f863384755d
Eugene Park <epark215@gmail.com>
2022-09-27 16:58:24 +0000

    Completed issue-1-user-can-view-list-of-characters

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 var characterList = document.querySelector('#character-list');
+var viewCharacters = document.querySelector('#view-characters');
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20');
 xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
+
   for (var i = 0; i < xhr.response.length; i++) {
-    var columnThirds = document.createElement('div');
-    columnThirds.setAttribute('class', 'column-thirds');
-    characterList.appendChild(columnThirds);
+    var style = document.createElement('div');
+    style.setAttribute('class', 'flex-basis justify-center padding');
+    characterList.appendChild(style);
+
+    var columnThreeFourths = document.createElement('div');
+    columnThreeFourths.setAttribute('class', 'column-three-fourths whitebackground borderradius');
+    style.appendChild(columnThreeFourths);
 
     var characterImage = document.createElement('img');
     characterImage.setAttribute('src', xhr.response[i].image);
     characterImage.setAttribute('class', 'image');
-    columnThirds.appendChild(characterImage);
+    columnThreeFourths.appendChild(characterImage);
 
     var characterName = document.createElement('p');
     characterName.setAttribute('class', 'character-name');
     characterName.textContent = xhr.response[i].name;
-    columnThirds.appendChild(characterName);
+    columnThreeFourths.appendChild(characterName);
   }
 });
 xhr.send();
+
+characterList.addEventListener('click', function () {
+  if (event.target.tagName === 'IMG') {
+    viewCharacters.setAttribute('class', 'hidden');
+  }
+});
