0dc2806ef7825409fc009f7c0811b110ab540e32
Eugene Park <epark215@gmail.com>
2022-09-27 22:24:14 +0000

    Revised xmlrequest function to target all characters and revised the loop to loop 20 times

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ var characterList = document.querySelector('#character-list');
 var viewCharacters = document.querySelector('#view-characters');
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20');
+xhr.open('GET', 'https://rickandmortyapi.com/api/character');
 xhr.responseType = 'json';
-xhr.addEventListener('load', function () {
 
-  for (var i = 0; i < xhr.response.length; i++) {
+xhr.addEventListener('load', function () {
+  for (var i = 0; i < 20; i++) {
     var style = document.createElement('div');
     style.setAttribute('class', 'flex-basis justify-center padding');
     characterList.appendChild(style);
@@ -16,13 +16,13 @@ xhr.addEventListener('load', function () {
     style.appendChild(columnThreeFourths);
 
     var characterImage = document.createElement('img');
-    characterImage.setAttribute('src', xhr.response[i].image);
+    characterImage.setAttribute('src', xhr.response.results[i].image);
     characterImage.setAttribute('class', 'image');
     columnThreeFourths.appendChild(characterImage);
 
     var characterName = document.createElement('p');
     characterName.setAttribute('class', 'character-name');
-    characterName.textContent = xhr.response[i].name;
+    characterName.textContent = xhr.response.results[i].name;
     columnThreeFourths.appendChild(characterName);
   }
 });
