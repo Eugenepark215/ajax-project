3384dda8bcba332263f1a599b5f6f1c0ec2e686e
Eugene Park <epark215@gmail.com>
2022-09-28 17:13:22 +0000

    Completed issue-2-user-can-view-details-of-specific-characters

--- js/main.js
+++ js/main.js
@@ -29,31 +29,29 @@ xhr.addEventListener('load', function () {
 });
 xhr.send();
 
-var xhrEpisodes = new XMLHttpRequest();
-xhrEpisodes.open('GET', 'https://rickandmortyapi.com/api/episode');
-xhrEpisodes.responseType = 'json';
-
 characterList.addEventListener('click', function () {
   if (event.target.tagName === 'IMG') {
     viewCharacters.setAttribute('class', 'hidden');
     for (var j = 0; j < xhr.response.results.length; j++) {
       if (xhr.response.results[j].image === event.target.src) {
         var style = document.createElement('div');
-        style.setAttribute('class', 'flex-basis justify-center padding');
+        style.setAttribute('id', 'style-information');
         characterInformation.appendChild(style);
 
-        var columnThreeFourths = document.createElement('div');
-        columnThreeFourths.setAttribute('class', 'column-three-fourths whitebackground borderradius');
-        style.appendChild(columnThreeFourths);
+        var columnFull = document.createElement('div');
+        columnFull.setAttribute('class', 'column-full whitebackground borderradius');
+        columnFull.setAttribute('id', 'column-for-information');
+        style.appendChild(columnFull);
 
         var characterImage = document.createElement('img');
         characterImage.setAttribute('src', xhr.response.results[j].image);
         characterImage.setAttribute('class', 'image');
-        columnThreeFourths.appendChild(characterImage);
+        characterImage.setAttribute('id', 'image-for-information');
+        columnFull.appendChild(characterImage);
 
         var row1 = document.createElement('div');
         row1.setAttribute('class', 'row justify-center');
-        columnThreeFourths.appendChild(row1);
+        columnFull.appendChild(row1);
 
         var characterName = document.createElement('h1');
         characterName.textContent = xhr.response.results[j].name;
@@ -61,35 +59,59 @@ characterList.addEventListener('click', function () {
 
         var row2 = document.createElement('div');
         row2.setAttribute('class', 'row');
-        columnThreeFourths.appendChild(row2);
+        columnFull.appendChild(row2);
 
-        var characterStatus = document.createElement('h3');
+        var characterStatus = document.createElement('h2');
         characterStatus.textContent = 'Status: ' + xhr.response.results[j].status;
+        characterStatus.setAttribute('class', 'margin-left');
         row2.appendChild(characterStatus);
 
         var row3 = document.createElement('div');
         row3.setAttribute('class', 'row');
-        columnThreeFourths.appendChild(row3);
+        columnFull.appendChild(row3);
 
-        var characterSpecies = document.createElement('h3');
+        var characterSpecies = document.createElement('h2');
         characterSpecies.textContent = 'Species: ' + xhr.response.results[j].species;
+        characterSpecies.setAttribute('class', 'margin-left');
         row3.appendChild(characterSpecies);
 
         var row4 = document.createElement('div');
         row4.setAttribute('class', 'row');
-        columnThreeFourths.appendChild(row4);
+        columnFull.appendChild(row4);
 
-        var characterGender = document.createElement('h3');
+        var characterGender = document.createElement('h2');
         characterGender.textContent = 'Gender: ' + xhr.response.results[j].gender;
-        row3.appendChild(characterGender);
+        characterGender.setAttribute('class', 'margin-left');
+        row4.appendChild(characterGender);
 
         var row5 = document.createElement('div');
         row5.setAttribute('class', 'row');
-        columnThreeFourths.appendChild(row5);
+        columnFull.appendChild(row5);
 
-        var characterLocation = document.createElement('h3');
+        var characterLocation = document.createElement('h2');
         characterLocation.textContent = 'Location: ' + xhr.response.results[j].location.name;
+        characterLocation.setAttribute('class', 'margin-left');
         row5.appendChild(characterLocation);
+
+        var row6 = document.createElement('div');
+        row6.setAttribute('class', 'row');
+        columnFull.appendChild(row6);
+
+        var episodesText = document.createElement('h2');
+        episodesText.textContent = 'Episodes:  ';
+        episodesText.setAttribute('class', 'margin-left');
+        row6.appendChild(episodesText);
+
+        var characterEpisode = document.createElement('h2');
+        for (var l = 0; l < xhr.response.results[j].episode.length; l++) {
+          if (l === xhr.response.results[j].episode.length - 1) {
+            characterEpisode.textContent += xhr.response.results[j].episode[l].slice(40);
+          } else {
+            characterEpisode.textContent += xhr.response.results[j].episode[l].slice(40) + ', ';
+          }
+          characterEpisode.setAttribute('class', 'margin-left');
+          row6.appendChild(characterEpisode);
+        }
       }
     }
   }
